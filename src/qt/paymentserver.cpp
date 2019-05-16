a2dc66a6ec4e4945a8c1ba82954db40cc2727eaa
abdielsudiro <50492010+abdielsudiro@users.noreply.github.com>
2019-05-16 08:06:15 +0700

    Update paymentserver.cpp

--- src/qt/paymentserver.cpp
+++ src/qt/paymentserver.cpp
@@ -33,7 +33,7 @@ const QString BITCOIN_IPC_PREFIX("ion:");
 //
 static QString ipcServerName()
 {
-    QString name(.MajacoinQt");
+    QString name("MajacoinQt");
 
     // Append a simple hash of the datadir
     // Note that GetDataDir(true) returns a different path
